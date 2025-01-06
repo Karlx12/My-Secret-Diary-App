e22a9b427c15dd4975839844882ad78549c3bb6b
RONALD FABRIZIO HURTADO RAMOS <maxx1212_2@hotmail.com>
2025-01-06 10:39:00 -0500

    Update: diary_home_page

--- lib/view/diary_home_page.dart
+++ lib/view/diary_home_page.dart
@@ -1,40 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:turn_page_transition/turn_page_transition.dart';
-import 'package:mysecretdiary/view/widget/diary_page_input.dart';
+import 'package:mysecretdiary/view/diary_pages_view.dart';
 
-class DiaryPagesView extends StatefulWidget {
-  const DiaryPagesView({super.key});
-
-  @override
-  DiaryPagesState createState() => DiaryPagesState();
-}
-
-class DiaryPagesState extends State<DiaryPagesView> {
-  final List<TextEditingController> _controllers = List.generate(
-    5, // Number of pages
-        (index) => TextEditingController(),
-  );
+class DiaryHomePage extends StatelessWidget {
+  const DiaryHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Secret Diary'),
-      ),
-      body: TurnPageView.builder(
-        itemCount: _controllers.length,
-        itemBuilder: (context, index) {
-          return DiaryPageInput(controller: _controllers[index]);
-        },
-      ),
+    return const Scaffold(
+      body: DiaryPagesView(),
     );
-  }
-
-  @override
-  void dispose() {
-    for (var controller in _controllers) {
-      controller.dispose();
-    }
-    super.dispose();
   }
 }
